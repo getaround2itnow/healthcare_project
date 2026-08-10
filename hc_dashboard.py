6cea49c593f9b909517b055058a0ac38ea1f6b14
getaround2itnow <sqreroot49@gmail.com>
2026-08-10 02:23:52 -0500

    Rename dashboard chart pdf & png charts - adding 1 pie pdf chart

--- hc_dashboard.py
+++ hc_dashboard.py
@@ -342,7 +342,7 @@ monthly_nurse_hours = (
     .reset_index()
 )
 
-# show a record count of 91 for each cms cert numb (ccn)
+# show a record count of 91 for each cms cert numb (ccn)cd
 pbj_quarters = (
     pbj_df
     .groupby(
